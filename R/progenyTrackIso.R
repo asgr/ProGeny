6871c79404536a3ea2c489a7bf20055ac02344ed
Aaron Robotham <aaron.robotham@uwa.edu.au>
2024-12-10 16:32:12 +0800

    Breaking it up into sub functions. Should make it easier...

--- R/progenyTrackIso.R
+++ R/progenyTrackIso.R
@@ -17,7 +17,7 @@ progenyTrackInterp = function(tracklist, target, logAge_vec=NULL, target_type='M
     target_orig = NULL
   }
 
-  track_bins = as.integer(cut(target, breaks=track_vals, right=FALSE))
+  track_bins = findInterval(x=target, vec=track_vals, all.inside=TRUE)
   track_bins[target == max(track_vals)] = length(track_vals) - 1L
 
   sel_track_bins = unique(track_bins)
@@ -221,6 +221,163 @@ progenyTrackInterp = function(tracklist, target, logAge_vec=NULL, target_type='M
   return(combine_out)
 }
 
+progenyTagFeatures = function(tracklist, deriv=1, n=1e4, expand=-1:1){
+  tracklist_temp = copy(tracklist)
+
+  if(!is.null(tracklist_temp$logZ)){
+    logZ_vals = unique(tracklist_temp$logZ)
+  }else{
+    tracklist_temp$logZ = -99
+    logZ_vals = -99
+  }
+
+  tracklist_temp[,tag:=FALSE]
+
+  logZ_use = NULL
+  Mini = NULL
+
+  for(logZ_use in logZ_vals){
+    i = NULL
+    target = NULL
+    track_vals = unique(tracklist_temp[logZ == logZ_use,Mini])
+    for(i in seq_along(track_vals)){
+      suppressWarnings({
+        sel = tracklist_temp[Mini == track_vals[i] & logZ == logZ_use, which=TRUE]
+
+        temp_func = splinefun(tracklist_temp[sel, list(logAge, Mass)])
+        Mass_root = rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(1,max(tracklist_temp[Mini == track_vals[i],logAge])), n=n)
+
+        temp_func = splinefun(tracklist_temp[sel, list(logAge, Lum)])
+        Lum_root = rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(1,max(tracklist_temp[Mini == track_vals[i],logAge])), n=n)
+
+        temp_func = splinefun(tracklist_temp[sel, list(logAge, Teff)])
+        Teff_root = rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(1,max(tracklist_temp[Mini == track_vals[i],logAge])), n=n)
+
+        temp_func = splinefun(tracklist_temp[sel, list(logAge, logG)])
+        logG_root = rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(1,max(tracklist_temp[Mini == track_vals[i],logAge])), n=n)
+
+        all_root = sort(unique(c(Mass_root, Lum_root, Teff_root, logG_root)))
+
+        tag_temp = findInterval(all_root, tracklist_temp[sel, logAge], all.inside = T)
+        tag_temp = unique(as.integer(outer(tag_temp, expand, FUN='+')))
+        tracklist_temp[sel, tag:= (1:length(sel) %in% tag_temp)]
+      })
+    }
+  }
+  return(tracklist_temp)
+}
+
+progenyMassMap = function(tracklist, logAge_vec){
+  tracklist_temp = copy(tracklist[tag == TRUE])
+  age_bin = findInterval(tracklist_temp$logAge, logAge_vec)
+  tracklist_temp = tracklist_temp[which(age_bin > 0),]
+  age_bin = age_bin[age_bin > 0]
+  age_bin_unique = sort(unique(age_bin))
+  Mini_vec = sort(unique(tracklist$Mini))
+
+  Mini_match = match(tracklist_temp$Mini, Mini_vec)
+  Mini_match_lo = Mini_match
+  Mini_match_lo[Mini_match_lo == 1L] = NA
+  Mini_match_hi = Mini_match
+  Mini_match_hi[Mini_match_hi == length(Mini_vec)] = NA
+
+  tracklist_temp[,Mini_prev:=Mini_vec[Mini_match_lo - 1L]]
+  tracklist_temp[,Mini_next:=Mini_vec[Mini_match_hi + 1L]]
+
+  for(i in age_bin_unique){
+    temp = tracklist_temp[which(age_bin == i),list(Mini, logAge)]
+    temp_Mini_hi = 10^unlist(temp[,log10(Mini) + (log10(Mini_next) - log10(Mini))*(logAge - logAge_vec[i]) / (logAge_vec[i + 1L] - logAge_vec[i])])
+    #Not worked on below yet
+    temp_Mini_lo = temp[,Mini + (Mini - Mini_prev)*(logAge - logAge_vec[i]) / (logAge_vec[i + 1L] - logAge_vec[i])]
+  }
+}
+
+progenyGradFeatures = function(tracklist, logAge_vec,
+                               feature_sample=seq(-0.01,0.01,len=5),
+                               deriv=1, n=1e4, keep_orig_mass=TRUE, cores=8){
+  tracklist_temp = copy(tracklist)
+  target_type = 'Mini'
+  target_log = TRUE
+
+  if(!is.null(tracklist_temp$logZ)){
+    logZ_vals = unique(tracklist_temp$logZ)
+  }else{
+    tracklist_temp$logZ = -99
+    logZ_vals = -99
+  }
+
+  cores = min(cores, length(logZ_vals), detectCores())
+  registerDoParallel(cores=cores)
+
+  logZ_use = NULL
+  Mini = NULL
+  final_out = foreach(logZ_use = logZ_vals)%dopar%{
+    i = NULL
+    target = NULL
+    track_vals = unique(tracklist_temp[logZ == logZ_use,Mini])
+    feature_out = foreach(i = 1:(length(track_vals) - 1L))%do%{
+    #for(i in 1:(length(track_vals) - 1L)){
+      temp_func = splinefun(tracklist_temp[Mini == track_vals[i] & logZ == logZ_use, list(10^logAge, log10(Lum))])
+      temp_logAge = log10(rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(0,10^max(tracklist_temp[Mini == track_vals[i],logAge])), n=n))
+      age_bins = as.integer(cut(temp_logAge, breaks=logAge_vec, right=FALSE))
+      age_bins = age_bins[!is.na(age_bins)]
+      sel_age_bins = unique(age_bins)
+
+      #Here we both log interpolate track upwards and downwards. The logic is there will be contributing features from both less and more massive stars at an interpolated value, e.g. a 13 Msol star will be (roughly) 50% a 10 Msol and 50% 20 Msol star, so need to map features from both directions to not miss important things.
+      temp_target_lo = {}
+      temp_target_hi = {}
+      for(bin in sel_age_bins){
+        #For fraction above the current mass track the next more massive track
+        temp_logAge_loc = temp_logAge[(temp_logAge >= logAge_vec[bin] & temp_logAge < logAge_vec[bin + 1L])]
+        temp_logAge_frac = (temp_logAge_loc - logAge_vec[bin])/(logAge_vec[bin + 1L] - logAge_vec[bin])
+        if(target_log){
+          temp_target_lo = c(temp_target_lo, 10^(log10(track_vals[i]) + temp_logAge_frac*(log10(track_vals[i + 1L]) - log10(track_vals[i]))))
+        }else{
+          temp_target_lo = c(temp_target_lo, track_vals[i] + temp_logAge_frac*(track_vals[i + 1L] - track_vals[i]))
+        }
+
+        if(bin > sel_age_bins[1]){
+          #For fraction below the current mass track to next less massive track
+          temp_logAge_loc = temp_logAge[(temp_logAge >= logAge_vec[bin - 1L] & temp_logAge < logAge_vec[bin]) | (bin == max(sel_age_bins) & temp_logAge == max(logAge_vec))]
+          temp_logAge_frac = (logAge_vec[bin] - temp_logAge_loc)/(logAge_vec[bin] - logAge_vec[bin - 1L])
+          if(target_log){
+            temp_target_hi = c(temp_target_hi, 10^(log10(track_vals[i + 1L]) - temp_logAge_frac*(log10(track_vals[i + 1L]) - log10(track_vals[i]))))
+          }else{
+            temp_target_hi = c(temp_target_hi, track_vals[i + 1L] - temp_logAge_frac*(track_vals[i + 1L] - track_vals[i]))
+          }
+        }
+      }
+
+      temp_target_lo = as.numeric(outer(temp_target_lo, feature_sample, '+'))
+      temp_target_hi = as.numeric(outer(temp_target_hi, feature_sample, '+'))
+
+      temp_DF_lo = data.table(Mini=temp_target_lo, logAge_lo=logAge_vec[bin], logAge_hi=logAge_vec[bin + 1L])
+      temp_DF_hi = data.table(Mini=temp_target_hi, logAge_lo=logAge_vec[bin - 1L], logAge_hi=logAge_vec[bin])
+
+      return(rbind(temp_DF_lo, temp_DF_hi))
+    }
+
+    feature_out = rbindlist(feature_out)
+    #this seems to be doing something sensible now...
+
+    feature_out = feature_out[Mini >= min(track_vals) & Mini <= max(track_vals),]
+
+    if(keep_orig_mass){
+      logAge_lo = logAge_vec[1:(length(logAge_vec) - 1L)]
+      logAge_hi = logAge_vec[2:length(logAge_vec)]
+      target = rep(track_vals, each=length(logAge_lo))
+      feature_out_add = data.table(Mini=target, logAge_lo=logAge_lo, logAge_hi=logAge_hi)
+      feature_out = rbind(feature_out, feature_out_add)
+    }
+    return(feature_out)
+  }
+
+  final_out = rbindlist(final_out)
+  final_out= unique(final_out)
+  setkey(final_out, logAge_lo, Mini)
+  return(final_out)
+}
+
 #This will extract the locations of important features
 #Next step is then to log interpolate to find the implied mass they would occur at given a logAge_vec
 .progenyLabelFeatures = function(tracklist, logAge_vec, feature_sample=seq(-0.01,0.01,len=5)){
@@ -237,81 +394,4 @@ progenyTrackInterp = function(tracklist, target, logAge_vec=NULL, target_type='M
   feature_mass = sort(c(unique(tracklist$Mini), as.numeric(outer(feature_mass, feature_sample, '+'))))
 
   return(feature_mass)
-}
-
-progenyGradFeatures = function(tracklist, logAge_vec, target_type='Mini',
-                               feature_sample=seq(-0.01,0.01,len=5),
-                               deriv=1, n=1e4, keep_orig_mass=TRUE){
-  track_vals = unique(tracklist[[target_type]])
-
-  if(target_type == 'Mini'){
-    target_log = TRUE
-  }else if(target_type == 'logZ'){
-    target_log = FALSE
-  }else{
-    stop('target type must be one of Mini or logZ')
-  }
-
-  i = NULL
-  target = NULL
-  feature_out = foreach(i = 1:(length(track_vals) - 1L))%do%{
-  #for(i in 1:(length(track_vals) - 1L)){
-    temp_func = splinefun(tracklist[get(target_type)==track_vals[i], list(10^logAge, log10(Lum))])
-    temp_logAge = log10(rootSolve::uniroot.all(function(x){temp_func(x,deriv)}, c(0,10^max(tracklist[get(target_type)==track_vals[i],logAge])), n=n))
-    age_bins = as.integer(cut(temp_logAge, breaks=logAge_vec, right=FALSE))
-    age_bins = age_bins[!is.na(age_bins)]
-    sel_age_bins = unique(age_bins)
-
-    #Here we both log interpolate track upwards and downwards. The logic is there will be contributing features from both less and more massive stars at an interpolated value, e.g. a 13 Msol star will be (roughly) 50% a 10 Msol and 50% 20 Msol star, so need to map features from both directions to not miss important things.
-    temp_target_lo = {}
-    temp_target_hi = {}
-    for(bin in sel_age_bins){
-      #For fraction above the current mass track the next more massive track
-      temp_logAge_loc = temp_logAge[(temp_logAge >= logAge_vec[bin] & temp_logAge < logAge_vec[bin + 1L])]
-      temp_logAge_frac = (temp_logAge_loc - logAge_vec[bin])/(logAge_vec[bin + 1L] - logAge_vec[bin])
-      if(target_log){
-        temp_target_lo = c(temp_target_lo, 10^(log10(track_vals[i]) + temp_logAge_frac*(log10(track_vals[i + 1L]) - log10(track_vals[i]))))
-      }else{
-        temp_target_lo = c(temp_target_lo, track_vals[i] + temp_logAge_frac*(track_vals[i + 1L] - track_vals[i]))
-      }
-
-      if(bin > sel_age_bins[1]){
-        #For fraction below the current mass track to next less massive track
-        temp_logAge_loc = temp_logAge[(temp_logAge >= logAge_vec[bin - 1L] & temp_logAge < logAge_vec[bin]) | (bin == max(sel_age_bins) & temp_logAge == max(logAge_vec))]
-        temp_logAge_frac = (logAge_vec[bin] - temp_logAge_loc)/(logAge_vec[bin] - logAge_vec[bin - 1L])
-        if(target_log){
-          temp_target_hi = c(temp_target_hi, 10^(log10(track_vals[i + 1L]) - temp_logAge_frac*(log10(track_vals[i + 1L]) - log10(track_vals[i]))))
-        }else{
-          temp_target_hi = c(temp_target_hi, track_vals[i + 1L] - temp_logAge_frac*(track_vals[i + 1L] - track_vals[i]))
-        }
-      }
-    }
-
-    temp_target_lo = as.numeric(outer(temp_target_lo, feature_sample, '+'))
-    temp_target_hi = as.numeric(outer(temp_target_hi, feature_sample, '+'))
-
-    temp_DF_lo = data.table(target=temp_target_lo, logAge_lo=logAge_vec[bin], logAge_hi=logAge_vec[bin + 1L])
-    temp_DF_hi = data.table(target=temp_target_hi, logAge_lo=logAge_vec[bin - 1L], logAge_hi=logAge_vec[bin])
-
-    return(rbind(temp_DF_lo, temp_DF_hi))
-  }
-
-  feature_out = rbindlist(feature_out)
-  #this seems to be doing something sensible now...
-
-  feature_out = feature_out[target >= min(track_vals) & target <= max(track_vals),]
-
-  if(keep_orig_mass){
-    logAge_lo = logAge_vec[1:(length(logAge_vec) - 1L)]
-    logAge_hi = logAge_vec[2:length(logAge_vec)]
-    target = rep(track_vals, each=length(logAge_lo))
-    feature_out_add = data.table(target=target, logAge_lo=logAge_lo, logAge_hi=logAge_hi)
-    feature_out = rbind(feature_out, feature_out_add)
-  }
-
-  setkey(feature_out, logAge_lo, target)
-
-  names(feature_out)[1] = target_type
-
-  return(feature_out)
 }

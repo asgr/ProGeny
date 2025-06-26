415ad488774768705993fc5222e5acd41ad9eb45
Aaron Robotham <aaron.robotham@uwa.edu.au>
2025-06-26 14:19:24 +0800

    Version change. Some other package checks added.

--- R/shinyProGeny.R
+++ R/shinyProGeny.R
@@ -2,6 +2,18 @@ runShinyProGeny = function(app_mode = 'user',
   iso_path = "~/Google Drive/My Drive/ProGeny_isochrone/",
   atmos_path = "~/Google Drive/My Drive/ProGeny_atmos/",
   cores_server = 4){
+    if(!requireNamespace("ProSpect", quietly = TRUE)){
+        stop("Need ProSpect package for runShinyProGeny to work! Get from GitHub asgr/ProSpect")
+    }
+
+    if(!requireNamespace("Rfits", quietly = TRUE)){
+        stop("Need Rfits package for runShinyProGeny to work! Get from GitHub asgr/Rfits")
+    }
+
+    if(!requireNamespace("magicaxis", quietly = TRUE)){
+        stop("Need magicaxis package for runShinyProGeny to work! Get from GitHub asgr/magicaxis")
+    }
+
     if(!requireNamespace("shiny", quietly = TRUE)){
         stop("Need shiny package for runShinyProGeny to work! Get from CRAN.")
     }
@@ -10,17 +22,21 @@ runShinyProGeny = function(app_mode = 'user',
         stop("Need shinyFiles package for runShinyProGeny to work! Get from CRAN.")
     }
 
+    if(!requireNamespace("shinybusy", quietly = TRUE)){
+        stop("Need shinybusy package for runShinyProGeny to work! Get from CRAN.")
+    }
+
     if(!requireNamespace("fst", quietly = TRUE)){
         stop("Need fst package for runShinyProGeny to work! Get from CRAN.")
     }
-  
-    if(!requireNamespace("Rfits", quietly = TRUE)){
-      stop("Need Rfits package for runShinyProGeny to work! Get from GitHub asgr/Rfits")
+
+    if(!requireNamespace("fst", quietly = TRUE)){
+        stop("Need fst package for runShinyProGeny to work! Get from CRAN.")
     }
 
     options(shiny.maxRequestSize = 2000*1024^2)
     options(ProGeny_app_mode = app_mode)
-    
+
     if(app_mode == 'server'){
       options(ProGeny_iso_path = iso_path)
       options(ProGeny_atmos_path = atmos_path)
